132827b00e2430b3e93bc3c93790c97beab76d03
awu556 <austinw556@gmail.com>
2020-06-09 12:03:27 -0400

    setting up routes

---
+++ src/common/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">My Account</a>
+      <a href="#">Pokedex</a>
+      <a href="#">Help</a>
+    </nav>
+  );
+};
+
+export default header;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="#" component={} />
+    </Switch>
+  );
+};
+
+export default Routes;
