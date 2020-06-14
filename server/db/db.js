5811f67eed14963b689aaab3d55e8f47b3c8d206
awu556 <austinw556@gmail.com>
2020-06-14 13:12:57 -0400

    working on db

---
+++ server/db/db.js
@@ -0,0 +1,26 @@
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database("./pkgo.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the pkgo database.");
+
+  db.serialize(() => {
+    db.each(`SELECT pokemon FROM ref_pkdex WHERE generation=1;`, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log(row.id + "\t" + row.name);
+    });
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Close the database connection.");
+  });
+});
+
+module.exports = db;
